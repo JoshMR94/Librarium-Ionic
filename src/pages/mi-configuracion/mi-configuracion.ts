650985c85390881ebb5cca8070e1ae2a769aefbc
JoshMR94 <joshuamarram@gmail.com>
2017-10-12 14:43:23 +0100

    Tercer commit. Configuracion y cuenta añadidos. Funcionan decentemente.

--- src/pages/mi-configuracion/mi-configuracion.ts
+++ src/pages/mi-configuracion/mi-configuracion.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { ToastController, Toggle } from 'ionic-angular';
 
 @Component({
   selector: 'page-mi-configuracion',
   templateUrl: 'mi-configuracion.html'
 })
 export class MiConfiguracionPage {
-  constructor() {
 
+  notificaciones: Toggle;
+  recomendaciones: Toggle;
+  sesion: Toggle;
+
+  constructor(public toastCtrl: ToastController) {
+
+  }
+
+  guardarConfiguracion(){
+    if(this.notificaciones != undefined || this.recomendaciones != undefined 
+        || this.sesion != undefined){
+          this.showToast("bottom");
+    } else {
+      this.showToastUndefined("bottom");
+    }
+  }
+
+  showToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Configuracion Guardada',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
+  }
+
+  showToastUndefined(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Debe seleccionar alguna opción',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
   }
 }
--- src/pages/mi-cuenta/mi-cuenta.ts
+++ src/pages/mi-cuenta/mi-cuenta.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-mi-cuenta',
   templateUrl: 'mi-cuenta.html'
 })
 export class MiCuentaPage {
-  constructor() {
+  user: string = "";
+  pass: string = "";
 
+  constructor(public alertCtrl: AlertController) {
+
+  }
+
+  iniciarSesion(){
+    if(this.user != "" && this.pass != ""){
+      this.showAlert("Correcto", "Sesion iniciada.")
+    } else{
+      this.showAlert("Error", "Debe introducir un usuario y contraseña validos.");
+    }
+  }
+
+  showAlert(titulo: string, subtitulo: string) {
+    let alert = this.alertCtrl.create({
+      title: titulo,
+      subTitle: subtitulo,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 }
